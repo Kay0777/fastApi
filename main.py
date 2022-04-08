4e535affd012cf27e5bd736a173c9cc85f7be079
Kay0777 <ronin.kay0705@gmail.com>
2022-04-08 22:14:09 +0500

    Api with FastApi

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from uvicorn import run
 from fastapi import FastAPI
+from models import User
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+	CORSMiddleware
+)
 
 
 @app.get('/')
