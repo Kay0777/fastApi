41c68c1ec9e491ebba0c19d046fdbf198aa9cd87
Kay0777 <ronin.kay0705@gmail.com>
2022-04-09 16:24:16 +0500

    minor changes

--- config.py
+++ config.py
@@ -2,33 +2,40 @@ from configparser import ConfigParser
 from os.path import exists
 from typing import Optional
 
+
 def Config(which: Optional[str] = None):
-	config = ConfigParser()
+    config = ConfigParser()
 
-	config_filename = 'config.ini'
-	if not exists(config_filename):
-		print('Not found config file!!!')
-		return
+    config_filename = 'config.ini'
+    if not exists(config_filename):
+        print('********************************')
+        print('**  Not found config file!!!  **')
+        print('********************************')
+        return
 
-	config.read(config_filename)
+    config.read(config_filename)
 
-	if which is None or which not in config.sections():
-		print('Choose config:', config.sections())
-		return
-	
-	return {
-		'App': {
-			'app_name': config['App']['app_name'],
-			'secret_key': config['App']['secret_key'],
-			'host': config['App']['host'],
-			'port': int(config['App']['port']),
-			'reload': bool(config['App']['reload']),
-			'log_level': config['App']['log_level']
-		},
-		'DB': {
-			'host': config['DB']['host'],
-			'port': int(config['DB']['port']),
-			'username': config['DB']['username'],
-			'password': config['DB']['password']
-		}
-	}[which]
+    sections = config.sections()
+    if which is None or which not in sections:
+        print('******************************************')
+        print(f'**  Choose config: {sections} **')
+        print('******************************************')
+        print('Choose config:', )
+        return
+
+    return {
+        'App': {
+            'app_name': config['App']['app_name'],
+            'secret_key': config['App']['secret_key'],
+            'host': config['App']['host'],
+            'port': int(config['App']['port']),
+            'reload': bool(config['App']['reload']),
+            'log_level': config['App']['log_level']
+        },
+        'DB': {
+            'host': config['DB']['host'],
+            'port': int(config['DB']['port']),
+            'username': config['DB']['username'],
+            'password': config['DB']['password']
+        }
+    }[which]
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from uvicorn import run
-from fastapi import FastAPI
-from models import User
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
+from fastapi import Query
+from typing import Optional, List, Dict
+
+from uvicorn import run
+
+# Import using Routes
+# from routes import userRouter
 from config import Config
 
 app = FastAPI()
 app.add_middleware(
-	CORSMiddleware
+    CORSMiddleware
 )
 
 App = Config('App')
@@ -15,16 +20,26 @@ App = Config('App')
 
 @app.get('/')
 async def home():
-	return {'msg': 'Hello world! Home!!!'}
+    return {'msg': 'Hello world! Home!!!'}
+
+
+@app.get('/users')
+async def get_user_by_params(names: Optional[List[str]] = Query(None)):
+    if names:
+        print(names)
+        return {'msg': f'User names: {names}'}
+    return {'msg': 'All Users'}
+
 
 @app.get('/users/{user_id}')
-async def users(user_id: int):
-	return {'msg': f'Users {user_id}'}
+async def get_user_by_id(user_id: int):
+    return {'msg': f'Users {user_id}'}
+
 
 if __name__ == "__main__":
     run(
-    	f"main:{App['app_name']}",
-    	host=App['host'],
-    	port=App['port'],
-    	reload=App['reload'],
-    	log_level=App['log_level'])
+        f"main:{App['app_name']}",
+        host=App['host'],
+        port=App['port'],
+        reload=App['reload'],
+        log_level=App['log_level'])
